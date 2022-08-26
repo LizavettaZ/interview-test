64a4c46936d98dc30978ddc40c3528ce473a63b9
Elizaveta Zaraiskaya <99411751+LizavettaZ@users.noreply.github.com>
2022-08-26 12:47:35 +0300

    Created Loader

---
+++ src/Loader/Loader.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import classes from './loader.module.scss'
+
+
+const Loader = () => {
+  return (
+    <div className={classes.Loader}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loader
