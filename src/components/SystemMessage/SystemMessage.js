528eb74c92129e998595cb80a41ea9c56280f63f
Elizaveta Zaraiskaya <99411751+LizavettaZ@users.noreply.github.com>
2022-08-26 12:44:39 +0300

    Updated component

--- src/components/SystemMessage/SystemMessage.js
+++ src/components/SystemMessage/SystemMessage.js
@@ -7,9 +7,10 @@ const SystemMessage = ({ time }) => {
   return (
     <div className={classes.SystemMessage}>
       <p>
-        {`${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()  }.
-        ${date.getMonth() + 1 < 10 ? '0' + date.getMonth() : date.getMonth()}.
-        ${date.getFullYear()}`}
+        {`${date.getDate() < 10 ? '0' + date.getDate() : date.getDate()}.
+          ${date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1}.
+          ${date.getFullYear()}
+         `}
       </p>
     </div>
   )
