08286830479c8d4c3ecf8117c791448688158b1d
Elizaveta Zaraiskaya <99411751+LizavettaZ@users.noreply.github.com>
2022-08-26 12:45:15 +0300

    Updated component

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import NewMessage from '../NewMessage/NewMessage'
-import SystemMessage from '../SystemMessage/SystemMessage'
 import Time from '../Time/Time'
+import classes from './message.module.scss'
+import { Avatar } from '../Avatar'
 
 
 const Message = ({ chat }) => {
+  const { created_at: time, message, user } = chat
+  const { avatar, name, surname, you} = user
+  const cls = [
+    classes.Message,
+    you && classes.My
+  ]
 
   return (
-    <div>
-      <NewMessage />
-      <SystemMessage time={chat.created_at}/>
-      <Time />
+    <div className={cls.join(' ')}>
+      {!you && <Avatar src={avatar} size={'sm'} />}
+      <div className={classes.Message__description}>
+        {!you && <h6 className={classes.description__title}>{`${name} ${surname}`}</h6>}
+        <div className={classes.description__text}>
+          <p>{message}</p>
+          <Time time={time} my={you}/>
+        </div>
+      </div>
     </div>
   )
 }
