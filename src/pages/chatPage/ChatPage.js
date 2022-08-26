07ba9aa23bf5155e94b2088f42c1ab8fe8be81f9
Elizaveta Zaraiskaya <99411751+LizavettaZ@users.noreply.github.com>
2022-08-26 12:45:47 +0300

    Updated page

--- src/pages/chatPage/ChatPage.js
+++ src/pages/chatPage/ChatPage.js
@@ -1,17 +1,26 @@
 import React, { memo } from 'react'
 import { useSelector } from 'react-redux'
 import Message from '../../components/Message/Message'
+import NewMessage from '../../components/NewMessage/NewMessage'
+import SystemMessage from '../../components/SystemMessage/SystemMessage'
+import classes from './chatPage.module.scss'
+import Input from '../../components/Input/Input'
 
 
 const ChatPage = () => {
   const userChat = useSelector(state => state.chats.chat)
 
   return (
-    <>
-      {userChat.map((chat) => (
-        <Message key={chat.id} chat={chat} />
+    <div className={classes.chatPage}>
+      {userChat.reverse().map((chat) => (
+        <div key={chat.id}>
+          {<SystemMessage time={chat.created_at} />}
+          {chat.is_new && <NewMessage />}
+          <Message chat={chat} />
+        </div>
       ))}
-    </>
+      <Input />
+    </div>
   )
 }
 
